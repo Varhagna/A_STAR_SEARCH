effae75bac04665e74335ccd2cfd448b8306ba79
Vahagn Tovmasian <vahagntovmasian@gmail.com>
2022-11-01 00:50:51 -0700

    Add initial documentation, clean up main()

--- search.py
+++ search.py
@@ -1,10 +1,13 @@
 from logging import warn
 import queue
+from select import select
 
 
 def main():
     p_mode = input("Welcome to my 8-Puzzle Solver! Would you like to solve your own puzzle? (Y / N) ")
-    gen_puzzle(p_mode)
+    start = gen_puzzle(p_mode)
+    goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+    algorithm = select_algorithm(goal)
 
 
 def gen_puzzle(p_mode="N"):
@@ -16,8 +19,6 @@ def gen_puzzle(p_mode="N"):
 
         for i in range(0, 9):
             puzzle[int(i / 3)][i % 3] = int(input("%s\n%s\n%s\nNumber? " % (puzzle[0], puzzle[1], puzzle[2])))
-
-        display_puzzle(puzzle)
     else:
         p_difficulty = input("Please enter your desired default puzzle difficulty level! \n 0 - Trivial \n 1 - Easy \n 2 - Medium \n 3 - Hard \n 4 - Impossible \n")
 
@@ -32,31 +33,64 @@ def gen_puzzle(p_mode="N"):
         else:
             puzzle = [[5, 0, 2], [6, 1, 7], [4, 8, 3]]
 
+    display_puzzle(puzzle)
+    return puzzle
 
-def select_algorithm(puzzle):
+
+## Helper function for selecting algorithm
+
+
+def select_algorithm(goal):
     algo = input("Select a search heuristic: 0 - Uniform Cost \n 1 - Misplaced Tile \n 2 - Manhattan Distance")
     heuristic = ""
     if algo == 0:
         heuristic = lambda i: i[1]
     elif algo == 1:
-        heuristic = lambda i: i[1] + get_misplaced_tiles(i[0], puzzle)
+        heuristic = lambda i: i[1] + get_misplaced_tiles(i[0], goal)
     elif algo == 2:
-        heuristic = lambda i: i[1] + get_manhattan(puzzle)
+        heuristic = lambda i: i[1] + get_manhattan(i[0], goal)
     else:
         return
 
-def get_manhattan(state, goal):
 
+## Parameters: two matrices representing the current state and the goal state of the 8-puzzle matrix
+## Output: The sum of Manhattan distances of all misplaced tiles
+def get_manhattan(state, goal):
+    distance = 0
+
+    ## We know the goal state is when we have 1 - 3 in the first row, 4 - 6 in the second row, and 7-8, 0 in the third row.
+    # That means the following correspondance to indices can be made
+    # 1 = (0, 0), 2 = (0, 1), 3 = (0, 2), 4 = (1, 0), 5 = (1, 1), 6 = (1, 2), 7 = (2, 0), 8 = (2, 1), 0 = (2, 2)
+    # The Manhattan Distance between any two indices is defined as |i_2 - i_1| + |j_2 - j_1|
+    # We want to find the sum of the manhattan distances of all misplaced tiles.
+
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if state[i][j] != goal[i][j]:
+                goal_indices = index_mapper(state[i][j])
+                distance += abs(i - goal_indices[0]) + abs(j - goal_indices[1])
+    return distance
+
+
+## Parameters: an integer
+## Output: The indices of the position of the integer in the goal state of the 8-puzzle matrix.
+def index_mapper(value):
+    ## shift values over so 1 -> 0, 2 -> 1, ... 0 -> 8
+    value = (value - 1) % 9
+    ## use earlier algorithm to convert values of range(0, 9) to appropriate index in 2d list
+    ## ex 8 / 3 = 2, 8 % 3 = 2 -> (2, 2)
+    return (int(value / 3), value % 3)
+
+
+## Parameters: two matrices representing the current state and the goal state of the 8-puzzle matrix
+## Output: The number of tiles such that state[i][j] != goal[i][j]
 def get_misplaced_tiles(state, goal):
     count = 0
     for i in range(0, 3):
         for j in range(0, 3):
             if state[i][j] != goal[i][j]:
                 count += 1
-
     return count
-
-
 
 
 def display_puzzle(puzzle):
