2c1f7d7497925a6c5098f475a8f7f593e4e78f34
Vahagn Tovmasian <vahagntovmasian@gmail.com>
2022-10-31 23:22:04 -0700

    Initial Commit

---
+++ search.py
@@ -0,0 +1,10 @@
+import queue
+
+
+def main():
+    p_mode = input("Welcome to my 8-Puzzle Solver! Would you like to solve your own puzzle? (Y / N) ")
+    solve_puzzle(p_mode);
+
+def solve_puzzle(p_mode="N"):
+    if (p_mode == "Y")
+
